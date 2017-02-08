40be59a70c2274cd969e0ef43e7af905a94afb6f
German Emmanuel <pixeliger@gmail.com>
2017-02-08 08:25:41 -0600

    Logic update for 4th activity

--- resources/js/viajeEspacio.js
+++ resources/js/viajeEspacio.js
@@ -1,3 +1,61 @@
+/*
+* Default Piece Status,
+* Puzzle Solution Arrays
+*/
+var _states = {
+    space: {
+        piece1: {state:0, degree: 0}, piece2: {state:0, degree: 0},
+        piece3: {state:0, degree: 0}, piece4: {state:0, degree: 0},
+        piece5: {state:0, degree: 0}, piece6: {state:0, degree: 0},
+        piece7: {state:0, degree: 0}, piece8: {state:0, degree: 0}
+    },
+    plane: {
+        piece1: {state:0, degree: 0}, piece2: {state:0, degree: 0},
+        piece3: {state:0, degree: 0}, piece4: {state:0, degree: 0},
+        piece5: {state:0, degree: 0}, piece6: {state:0, degree: 0},
+        piece7: {state:0, degree: 0}, piece8: {state:0, degree: 0}
+    },
+    truck: {
+        piece1: {state:0, degree: 0}, piece2: {state:0, degree: 0},
+        piece3: {state:0, degree: 0}, piece4: {state:0, degree: 0},
+        piece5: {state:0, degree: 0}, piece6: {state:0, degree: 0},
+        piece7: {state:0, degree: 0}, piece8: {state:0, degree: 0}
+    },
+    unicorn: {
+        piece1: {state:0, degree: 0}, piece2: {state:0, degree: 0},
+        piece3: {state:0, degree: 0}, piece4: {state:0, degree: 0},
+        piece5: {state:0, degree: 0}, piece6: {state:0, degree: 0},
+        piece7: {state:0, degree: 0}, piece8: {state:0, degree: 0}
+    }
+}
+// Current Piece States
+var _c_states = {
+    space: {
+        piece1: {state:0, degree: 0}, piece2: {state:0, degree: 0},
+        piece3: {state:0, degree: 0}, piece4: {state:0, degree: 0},
+        piece5: {state:0, degree: 0}, piece6: {state:0, degree: 0},
+        piece7: {state:0, degree: 0}, piece8: {state:0, degree: 0}
+    },
+    plane: {
+        piece1: {state:0, degree: 0}, piece2: {state:0, degree: 0},
+        piece3: {state:0, degree: 0}, piece4: {state:0, degree: 0},
+        piece5: {state:0, degree: 0}, piece6: {state:0, degree: 0},
+        piece7: {state:0, degree: 0}, piece8: {state:0, degree: 0}
+    },
+    truck: {
+        piece1: {state:0, degree: 0}, piece2: {state:0, degree: 0},
+        piece3: {state:0, degree: 0}, piece4: {state:0, degree: 0},
+        piece5: {state:0, degree: 0}, piece6: {state:0, degree: 0},
+        piece7: {state:0, degree: 0}, piece8: {state:0, degree: 0}
+    },
+    unicorn: {
+        piece1: {state:0, degree: 0}, piece2: {state:0, degree: 0},
+        piece3: {state:0, degree: 0}, piece4: {state:0, degree: 0},
+        piece5: {state:0, degree: 0}, piece6: {state:0, degree: 0},
+        piece7: {state:0, degree: 0}, piece8: {state:0, degree: 0}
+    }
+}
+
 $(document).ready(function() {
     var _instructions       = $('#instructionsContent');
     var _buttons            = $('#btnsContent');
@@ -69,7 +127,7 @@ $(document).ready(function() {
             }, 500);
         }, 180);
     });
-    
+
     _unicornBtn.click(function(event) {
         _buttons.hide();
         _unicornPuzzle.fadeIn(300).show('fast');
@@ -80,10 +138,58 @@ $(document).ready(function() {
         }, 180);
     });
 
+    /*
+    * Rotation Buttons, catch every puzzle piece dynamically
+    *
+    */
 
+    $('.rotate-right').click(function(event) {
+        var current_piece   = $(this).siblings('.puzzle-piece').attr('id');
+        var _current        = current_piece.split('_');
+        var _puzzle         = _current[0];
+        var _piece          = _current[1];
+        //
+        _c_states[_puzzle][_piece].degree += 90;
+        _c_states[_puzzle][_piece].degree == 360 ? _c_states[_puzzle][_piece].degree = 0 : _c_states[_puzzle][_piece].degree = _c_states[_puzzle][_piece].degree;
+        //
+        var _degree         = _c_states[_puzzle][_piece].degree;
+
+        rotate(current_piece, _degree);
+    });
+
+    $('.rotate-left').click(function(event) {
+        var current_piece   = $(this).siblings('.puzzle-piece').attr('id');
+        var _current        = current_piece.split('_');
+        var _puzzle         = _current[0];
+        var _piece          = _current[1];
+        //
+        _c_states[_puzzle][_piece].degree -= 90;
+        _c_states[_puzzle][_piece].degree == (-360) ? _c_states[_puzzle][_piece].degree = 0 : _c_states[_puzzle][_piece].degree = _c_states[_puzzle][_piece].degree;
+        //
+        var _degree = _c_states[_puzzle][_piece].degree;
+
+        rotate(current_piece, _degree);
+    });
 
     $('.space-square').click(function(event) {
+        /*
+          #! Deselect all Pieces but current
+          #! Select Class for current Piece
+          #! Show Rotate buttons
+        */
         $(this).find('.rotate-right').toggleClass('show-buttons');
         $(this).find('.rotate-left').toggleClass('show-buttons');
     });
+
+    function rotate(piece, degree){
+        current_piece   = piece;
+        _degree         = degree;
+
+        $('#'+current_piece).css({
+            '-moz-transform'    : 'rotate('+_degree+'deg)',
+            '-webkit-transform' : 'rotate('+_degree+'deg)',
+            'transform'         : 'rotate('+_degree+'deg)'
+        });
+        // Compare Array states => Piece ON Class
+    }
 });
